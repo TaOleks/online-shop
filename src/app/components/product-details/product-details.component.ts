61c358c45d4f8c93bac98cd63097f40589cc972c
TaOleks <tahtay.aleksey@gmail.com>
2023-08-06 18:50:12 +0200

    Details of product
    ng g resolver
    setting rout

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { IProducts } from 'src/app/models/products';
 
 @Component({
   selector: 'app-product-details',
@@ -6,5 +9,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent {
+  product!: IProducts;
+  productsSubscription!:Subscription;
+  constructor (private route:ActivatedRoute){}
 
+  ngOnInit():void{
+    this.productsSubscription=this.route.data.subscribe((data) =>{
+      this.product = data['data']
+    })
+  }
 }
