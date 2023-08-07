33aa908051b9e35f794d4ffb58bd51c4e838b483
TaOleks <tahtay.aleksey@gmail.com>
2023-08-07 16:46:13 +0200

    Create button  Add product

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -10,10 +10,16 @@ import { ProductsService } from 'src/app/services/products.service';
 })
 export class ProductsComponent implements OnInit {
 products!:IProducts[];
-productsSubscription!: Subscription
+productsSubscription!: Subscription;
+canEdit:boolean =false;
+canView: boolean=false;
+
 constructor(private PraductsService:ProductsService){}
 
 ngOnInit():void{
+// ... here ligic for autorization and if everything ok =>
+   this.canEdit=true
+
   this.productsSubscription = this.PraductsService.getProducts().subscribe((data)=>{
     this.products = data;
   })
